a4e477100e2c2a4b30f87b23b11e0a63f0b360c3
Alius999 <myzikant999@gmail.com>
2022-12-05 20:58:07 +0500

    Исправляет замечения код ревью

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import Inputmask from './inputmask';
+import JustValidate from './just-validate.es';
 
 // ---------------------------------
 
@@ -47,27 +49,31 @@ window.addEventListener('DOMContentLoaded', () => {
 
 const header = document.querySelector('.header');
 
-const accordeonButton = document.querySelector('.footer-menu__accordeon-button');
+const accordeonButton = document.querySelector('.footer-menu__control');
+const plusButton = document.querySelector('.footer-menu__control-open');
+const minusButton = document.querySelector('.footer-menu__control-close');
 const footerList = document.querySelector('.footer-menu__list');
 
-const plusButton = document.querySelector('.footer-menu__accordeon-plus');
-const minusButton = document.querySelector('.footer-menu__accordeon-minus');
-
-const accordeonButtonContacts = document.querySelector('.footer-contacts__accordeon-button');
+const accordeonIcon = document.querySelector('.footer-contacts__accordeon-icon');
 const contactsList = document.querySelector('.footer-contacts__list');
-
-const plusButtonContacts = document.querySelector('.footer-contacts__accordeon-plus');
-const minusButtonContacts = document.querySelector('.footer-contacts__accordeon-minus');
+const plusButtonContacts = document.querySelector('.footer-contacts__plus');
+const minusButtonContacts = document.querySelector('.footer-contacts__minus');
 
 const aboutButton = document.querySelector('.about__button');
-const aboutText = document.querySelector('.modal-form__close');
+const aboutText = document.querySelector('.modal-window__close');
 const textAfterDivider = document.querySelector('.about__text--after-divider');
 const textAfterDividerMobile = document.querySelector('.about__text--after-divider-mobile');
 
-const closeModal = document.querySelector('.modal-form__close');
-const modalWindow = document.querySelector('.modal-form');
+const closeModal = document.querySelector('.modal-window__close');
+const modalWindow = document.querySelector('.modal-window');
 const headerCallBack = document.querySelector('.header__calback');
-const modalWrapper = document.querySelector('.modal-form__wrapper');
+const modalWrapper = document.querySelector('.modal-window__wrapper');
+
+const goodsHeader = document.querySelector('.goods__header');
+const goodsHeaderMobile = goodsHeader.dataset.mobileText;
+
+const firstScreenButton = document.querySelector('.first-screen__container-button');
+const firstScreenButtonMobile = firstScreenButton.dataset.firstScreenButtonMobile;
 
 // Без JS
 
@@ -83,7 +89,7 @@ im.mask(selector);
 
 // Маска телефона для модального окна
 
-let selectorModal = document.querySelector('.modal-form__phone');
+let selectorModal = document.querySelector('.modal-window__phone');
 let imModal = new Inputmask('+7 (999) 999-99-99');
 
 imModal.mask(selectorModal);
@@ -93,74 +99,74 @@ imModal.mask(selectorModal);
 const validation = new JustValidate('#feedback-form');
 
 validation
-  .addField('#feedback-form__name', [
-    {
-      rule: 'minLength',
-      value: 3,
-      errorMessage: 'В имени должно быть больше трёх букв',
-    },
-    {
-      rule: 'maxLength',
-      value: 30,
-    },
-    {
-      rule: 'required',
-      errorMessage: 'Укажите Ваше имя',
-    },
-  ])
-  .addField('#agreement', [
-    {
-      rule: 'required',
-      errorMessage: 'Вы должны согласиться с правилами',
-    },
-  ])
-  .addField('#feedback-form__phone', [
-    {
-      rule: 'required',
-      errorMessage: 'Укажите Ваш телефон',
-    },
-    {
-      rule: 'maxLength',
-      value: 18,
-    },
-  ]);
+    .addField('#feedback-form__name', [
+      {
+        rule: 'minLength',
+        value: 3,
+        errorMessage: 'В имени должно быть больше трёх букв',
+      },
+      {
+        rule: 'maxLength',
+        value: 30,
+      },
+      {
+        rule: 'required',
+        errorMessage: 'Укажите Ваше имя',
+      }
+    ])
+    .addField('#agreement', [
+      {
+        rule: 'required',
+        errorMessage: 'Вы должны согласиться с правилами',
+      }
+    ])
+    .addField('#feedback-form__phone', [
+      {
+        rule: 'required',
+        errorMessage: 'Укажите Ваш телефон',
+      },
+      {
+        rule: 'maxLength',
+        value: 18,
+      }
+    ]);
 
 // Валидация модального окна
 
-const validationModal = new JustValidate('#modal-form');
+const validationModal = new JustValidate('#modal-window');
 
 validationModal
-  .addField('#modal-form__name', [
-    {
-      rule: 'minLength',
-      value: 3,
-      errorMessage: 'В имени должно быть больше трёх букв',
-    },
-    {
-      rule: 'maxLength',
-      value: 30,
-    },
-    {
-      rule: 'required',
-      errorMessage: 'Укажите Ваше имя',
-    },
-  ])
-  .addField('#modal-agreement', [
-    {
-      rule: 'required',
-      errorMessage: 'Вы должны согласиться с правилами',
-    },
-  ])
-  .addField('#modal-form__phone', [
-    {
-      rule: 'required',
-      errorMessage: 'Укажите Ваш телефон',
-    },
-    {
-      rule: 'maxLength',
-      value: 18,
-    },
-  ]);
+    .addField('#modal-window__name', [
+      {
+        rule: 'minLength',
+        value: 3,
+        errorMessage: 'В имени должно быть больше трёх букв',
+      },
+      {
+        rule: 'maxLength',
+        value: 30,
+      },
+      {
+        rule: 'required',
+        errorMessage: 'Укажите Ваше имя',
+      }
+    ])
+    .addField('#modal-agreement', [
+      {
+        rule: 'required',
+        errorMessage: 'Вы должны согласиться с правилами',
+      }
+    ])
+    .addField('#modal-window__phone', [
+      {
+        rule: 'required',
+        errorMessage: 'Укажите Ваш телефон',
+      },
+      {
+        rule: 'maxLength',
+        value: 18,
+      }
+    ]);
 
 // Фиксация header в desktop
 
@@ -184,7 +190,7 @@ accordeonButton.addEventListener('click', () => {
   footerList.classList.toggle('footer-menu__display');
 });
 
-accordeonButtonContacts.addEventListener('click', () => {
+accordeonIcon.addEventListener('click', () => {
   if (footerList.classList.contains('footer-menu__display')) {
     plusButton.style['display'] = '';
     minusButton.style['display'] = '';
@@ -200,7 +206,7 @@ accordeonButtonContacts.addEventListener('click', () => {
 
 // Регулировка текста о компании
 
-if(window.matchMedia('(max-width: 768px)').matches) {
+if (window.matchMedia('(max-width: 768px)').matches) {
   aboutButton.addEventListener('click', () => {
     aboutText.classList.toggle('about__click');
     if (aboutText.classList.contains('about__click')) {
@@ -215,8 +221,13 @@ if(window.matchMedia('(max-width: 768px)').matches) {
   });
 }
 
+if (window.matchMedia('(max-width: 1024px)').matches) {
+  goodsHeader.textContent = goodsHeaderMobile;
+  firstScreenButton.textContent = firstScreenButtonMobile;
+}
 
-if(window.matchMedia('(min-width: 768px)').matches) {
+
+if (window.matchMedia('(min-width: 768px)').matches) {
   aboutButton.addEventListener('click', () => {
     aboutText.classList.toggle('about__click');
     if (aboutText.classList.contains('about__click')) {
@@ -233,7 +244,7 @@ if(window.matchMedia('(min-width: 768px)').matches) {
 
 headerCallBack.addEventListener('click', () => {
   modalWindow.style.display = 'block';
-  document.getElementById('modal-form__name').focus();
+  document.getElementById('modal-window__name').focus();
 });
 
 closeModal.addEventListener('click', () => {
@@ -243,7 +254,7 @@ closeModal.addEventListener('click', () => {
 modalWindow.addEventListener('click', (e) => {
   const withinBoundaries = e.composedPath().includes(modalWrapper);
 
-	if ( ! withinBoundaries ) {
-		modalWindow.style.display = 'none'; // Cкрываем элемент т.к. клик был за его пределами
-	}
+  if (!withinBoundaries) {
+    modalWindow.style.display = 'none'; // Cкрываем элемент т.к. клик был за его пределами
+  }
 });
